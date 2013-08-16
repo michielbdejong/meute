f12ed264dd545c9f6de1a45bc7dd8cd115729da2
Michiel de Jong <michiel@unhosted.org>
2013-08-16 13:45:38 +0200

    sockethub-client.js example app

---
+++ config.sample.js
@@ -0,0 +1,30 @@
+var config = {
+  connect: {
+    host: 'localhost',
+    ssl: true,
+    register: {
+      secret: ''
+    }
+  },
+  facebook: {
+    access_token: ''
+  },
+  twitter: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
+  },
+  email: {
+    smtp: {
+      username: 'user@example.com',
+      password: '',
+      host: 'smtp.example.com'
+    },
+    imap: {
+      username: 'user@example.com',
+      password: '',
+      host: 'imap.example.com'
+    }
+  }
+};
---
+++ contacts.js
@@ -0,0 +1,17 @@
+var contacts = {
+  'Twitter (my public stream)': {
+    platform: 'twitter',
+    verb: 'post',
+    target: 'me'
+  },
+  'Facebook (my public stream)': {
+    platform: 'facebook',
+    verb: 'post',
+    target: 'me'
+  },
+  'anything@michielbdejong.com (email)': {
+    platform: 'email',
+    verb: 'send',
+    target: 'anything@michielbdejong.com'
+  }
+};
