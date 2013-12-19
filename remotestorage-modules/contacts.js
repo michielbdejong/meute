88ab98396b8dfaeb3cdf0b5a34dda3eac0d45276
Michiel@unhosted <michiel@unhosted.org>
2013-12-19 14:07:47 +0530

    move PrefixTree, SyncedVar and SyncedMap into module-utils.js

--- remotestorage-modules/contacts.js
+++ remotestorage-modules/contacts.js
@@ -5,47 +5,6 @@ RemoteStorage.defineModule('contacts', function(privateClient, publicClient) {
   function genUuid() {
     return Math.random()+'-'+(new Date().getTime().toString())+Math.random();
   }
-  function SyncedVar(name, baseClient) {
-    var data;
-    baseClient.on('change', function(e) {
-      e.key = e.relativePath.substring('contacts/'.length);
-      console.log('change', e);
-      if(e.key==name) {
-        data = e.newValue;
-      }
-    });
-    return {
-      get: function() {
-        return data;
-      },
-      set: function(val) {
-        baseClient.storeObject('SyncedVar', name, val);
-        data=val;
-      }
-    };
-  }
-  function SyncedMap(name, baseClient) {
-    var data = {}, prefixTree = PrefixTree(baseClient.scope(name+'/'));
-    //prefixTree.cache('');
-    prefixTree.on('change', function(e) {
-      console.log('change', e);
-      if(e.key.substring(0, name.length+1) == name + '/') {
-        data[e.key.substring(name.length+1)] = e.newValue;
-      }
-    });
-    return {
-      get: function(key) {
-        return data[key];
-      },
-      set: function(key, val) {
-        prefixTree.storeObject('SyncedMapKey', key, val);
-        data[key]=val;
-      },
-      getKeys: function() {
-        return Object.getOwnPropertyNames(data);
-      }
-    };
-  }
   return {
     exports: {
       add: function(name, details) {
