750b14e4ee93ce6825843b4a548c8c8b8afda198
Michiel@unhosted <michiel@unhosted.org>
2014-03-27 15:00:44 +0530

    add in missing modules

---
+++ upstream/remotestorage-modules/email.js
@@ -0,0 +1,19 @@
+RemoteStorage.defineModule('email', function(privClient, pubClient) {
+
+  function setConfig(pwd, config) {
+    privClient.storeFile('application/json', 'email-config', 
+        sjcl.encrypt(pwd, JSON.stringify(config)));
+  }
+  function getConfig(pwd) {
+    return privClient.getFile('email-config').then(function(a) {
+      a.data = sjcl.decrypt(pwd, a.data);
+      return a;
+    });
+  }
+  return {
+    exports: {
+      setConfig: setConfig,
+      getConfig: getConfig
+    }
+  };
+});
---
+++ upstream/remotestorage-modules/twitter.js
@@ -0,0 +1,19 @@
+RemoteStorage.defineModule('twitter', function(privClient, pubClient) {
+
+  function setConfig(pwd, config) {
+    privClient.storeFile('application/json', 'twitter-config', 
+        sjcl.encrypt(pwd, JSON.stringify(config)));
+  }
+  function getConfig(pwd) {
+    return privClient.getFile('twitter-config').then(function(a) {
+      a.data = sjcl.decrypt(pwd, a.data);
+      return a;
+    });
+  }
+  return {
+    exports: {
+      setConfig: setConfig,
+      getConfig: getConfig
+    }
+  };
+});
