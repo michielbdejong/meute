c4e8f7ea9086ec106e158c3863b57d4f3344344f
Michiel@unhosted <michiel@unhosted.org>
2014-04-20 14:47:48 +0400

    add functions from v0.1.0 to email modules

--- upstream/remotestorage-modules/email.js
+++ upstream/remotestorage-modules/email.js
@@ -4,10 +4,94 @@ RemoteStorage.defineModule('email', function(privClient, pubClient) {
   }
   var credentialsStore = CredentialsStore('email', privClient);
 
+  function mergeObjects(existing, adding) {
+    var i;
+    if((typeof(adding) != 'object') || (typeof(existing) != 'object')) {
+      return existing;
+    }
+    if(Array.isArray(existing)) {
+      if(Array.isArray(adding)) {
+        for(i=0; i<adding.length; i++) {
+          if(existing.indexOf(adding[i]) == -1) {
+            existing.push(adding[i]);
+          }
+        }
+        return existing;
+      } else {
+        return Array.concat(existing, [adding]);
+      }
+    }
+    if(Array.isArray(adding)) {
+      return existing;
+    }
+    for(i in adding) {
+      if(typeof(adding[i]=='object') && typeof(existing[i])) {
+        existing[i]=mergeObjects(existing[i], adding[i]);
+      }
+      if(!existing[i]) {
+        existing[i] = adding[i];
+      }
+    }
+    return existing;
+  }
+
+  var messages = PrefixTree(privateClient.scope('messages/'));
   return {
     exports: {
+      _init: function() {
+        privateClient.cache('', 'ALL');
+      },
+      getMessage: function(msgId) {
+        return messages.getObject(msgId);
+      },
+      getMessageIds: function(prefix) {
+        return messages.getKeysAndDirs(prefix || '');
+      },
+      getImapBoxIndex: function(account, box) {
+        return remoteStorage.email.priv.getAll('imap/'+account+'/'+box+'/');
+      },
+      getNextMissingSequence: function(account, box) {
+        return remoteStorage.email.priv.getAll('imap/'+account+'/'+box+'/').then(
+          function(map) {
+            var i, highest=0, start;
+            for(i in map) {
+              if(parseInt(i)>highest) {
+                highest = i;
+              }
+              console.log('highest? ', i, highest);
+            }
+            for(i=highest;i>=0;i--) {
+              if(map[i] || i===0) {
+                if(start) {
+                  return 'highest:'+highest+', start:'+start+', end:'+(parseInt(i)+1)
+                    +', suggestion: document.fetchEmails('+(1+Math.floor((highest-start)/10))+', '+10+', false);';
+                }
+              } else {
+                if(!start) {
+                  start = parseInt(i);
+                }
+              }
+            }
+            return 'up to date! highest:'+highest+', start:'+start+', end:'+(parseInt(i)+1)+', map:'+JSON.stringify(map);
+          }
+        );
+      },
+      storeMessage: function(msgId, obj, accountName) {
+        var existing = messages.getObject(msgId) || {},
+          merge = JSON.parse(JSON.stringify(mergeObjects(existing, obj)));//to avoid DataCloneError
+        //console.log('merged', existing, obj, merge);
+        return messages.storeObject('message', msgId, merge).then(function() {
+          return privateClient.storeObject('imapSeqno-to-messageId', 'imap/'+obj.object.imapAccountName+'/'+obj.object.imapBoxName+'/'+obj.object.imapSeqNo, {
+            account: accountName,
+            box: 'INBOX',
+            seqNo: obj.object.imapSeqNo,
+            messageId: msgId
+          });
+        });
+      },
       setConfig: credentialsStore.setConfig,
-      getConfig: credentialsStore.getConfig
+      getConfig: credentialsStore.getConfig      
     }
   };
 });
+remoteStorage.email._init();
