ceb6006bc541f2c038f40c429e67baca99862a62
Michiel de Jong <michiel@unhosted.org>
2014-10-21 16:36:11 +0100

    lower waiting times

--- butler.js
+++ butler.js
@@ -54,23 +54,23 @@ function beginApp() {
   setTimeout(function() {
     console.log('connecting to sockethub:');
     meute.addAccount('sockethub', 'ws://localhost:10550/', '1234567890');
-  }, 1000);
+  }, 10);
 
   setTimeout(function() {
     console.log('connecting to irc:');
     meute.addAccount('irc', 'irc.freenode.net', 'meute-butler');
-  }, 3000);
+  }, 30);
 
   setTimeout(function() {
     console.log('joining #meute:');
     meute.join('irc', '#meute');
-  }, 13000);
+  }, 130);
   setTimeout(function() {
     console.log('saying hi:');
     meute.send('irc', '#meute', 'The butler did it!');
-  }, 23000);
+  }, 230);
   setTimeout(function() {
     console.log('leaving room:');
     meute.leave('irc', '#meute');
-  }, 25000);
+  }, 250);
 }
