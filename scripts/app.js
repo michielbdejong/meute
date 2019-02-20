82d97a98e4be372f352b721055a549cfcc778ed4
Michiel de Jong <michiel@unhosted.org>
2019-02-20 13:28:39 +0100

    allow adding a message, fix #30

---
+++ scripts/app.js
@@ -0,0 +1,107 @@
+  ////////////
+ // Basics //
+////////////
+const ns = {
+  dct: $rdf.Namespace('http://purl.org/dc/terms/'),
+  foaf: $rdf.Namespace('http://xmlns.com/foaf/0.1/'),
+  ldp: $rdf.Namespace('http://www.w3.org/ns/ldp#'),
+  sioc: $rdf.Namespace('http://rdfs.org/sioc/ns#'),
+  wf: $rdf.Namespace('http://www.w3.org/2005/01/wf/flow#'),
+};
+const store = $rdf.graph();
+const fetcher = new $rdf.Fetcher(store);
+const updater = new $rdf.UpdateManager(store);
+window.store = store; // for live debugging
+
+  /////////
+ // Vue //
+/////////
+const app = new Vue({
+  el: '#app',
+  data: {
+    chats: [],
+    webId: '',
+  }
+});
+
+  ///////////
+ // WebId //
+///////////
+solid.auth.trackSession(session => {
+  if (!session) {
+    document.getElementById('login-state').innerHTML = 'Not logged in.';
+  } else {
+    app.webId = session.webId;
+    document.getElementById('login-state').innerHTML = `Logged in as ${session.webId}.`;
+  }
+})
+function logout() {
+  solid.auth.logout();
+}
+async function login() {
+  let session = await solid.auth.currentSession();
+  let popupUri = 'https://solid.community/common/popup.html';
+  if (!session)
+    session = await solid.auth.popupLogin({ popupUri });
+  alert(`Logged in as ${session.webId}`);
+}
+
+  ///////////////////////
+ // Send chat message //
+///////////////////////
+// from https://github.com/solid/solid-ui/blob/master/src/messageArea.js#L77-L112:
+function sendMessage (chatUrl, text, me) {
+  var sts = [];
+  var now = new Date();
+  var timestamp = '' + now.getTime();
+  var dateStamp = $rdf.term(now);
+  var conversation = store.sym(chatUrl + '#this');
+  // http://www.w3schools.com/jsref/jsref_obj_date.asp
+  var message = store.sym(chatUrl + '#' + 'Msg' + timestamp);
+  var messageStore = conversation.doc();
+  sts.push(new $rdf.Statement(conversation, ns.wf('message'), message, messageStore));
+  sts.push(new $rdf.Statement(message, ns.sioc('content'), store.literal(text), messageStore))
+  sts.push(new $rdf.Statement(message, ns.dct('created'), dateStamp, messageStore))
+  sts.push(new $rdf.Statement(message, ns.foaf('maker'), me, messageStore))
+
+  var sendComplete = function (uri, success, body) {
+    if (!success) {
+      console.log('Error writing message: ' + body);
+    } else {
+      console.log('Succcess writing message');
+    }
+  }
+  updater.update([], sts, sendComplete)
+}
+
+function newChatMsg(index) {
+  return sendMessage(app.chats[index].url, app.chats[index].newMsg, app.webId);
+}
+
+  ////////////////////
+ // Fetch chat log //
+////////////////////
+async function refreshChatList(chatsIndexUrl) {
+  // Load the person's hosted chats into the store
+  await fetcher.load(chatsIndexUrl);
+  const chatsList = store.match(null, ns.ldp("contains")).map(e => e.object.value + 'index.ttl').map(displayChat);
+}
+async function displayChat(chatUrl) {
+  await fetcher.load(chatUrl);
+  const chatObj  = {
+    url: chatUrl,
+    messages: [],
+    newMsg: ''
+  };
+  const messageObjects = store.match(null, ns.sioc('content'), null, $rdf.sym(chatUrl).doc());
+  messageObjects.map(obj => {
+    const sub = obj.subject;
+    const maker = store.match(sub, $rdf.Namespace('http://xmlns.com/foaf/0.1/')('maker'));
+    console.log(obj.object.value, 'made by', maker[0].object.value, store.match(maker[0].object.value));
+    chatObj.messages.push({
+      maker: maker[0].object.value,
+      text: obj.object.value
+    });
+  });
+  app.chats.push(chatObj);
+}
